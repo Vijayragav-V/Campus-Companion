71bdd172ea9f44a4b69926c2371ac11d2a39961f
Vijay Varadarajan <151465767+Vijayragav-V@users.noreply.github.com>
2024-01-03 21:21:39 -0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,4 +1,4 @@
-KEY='82e4ea11d7910b2819381193a78d819306575e9a2' # Your Key Here
-SECRET='a80ec4f0300147f9357f423fa81feef7' # Your Secret Here
-USER_ID='76781528' # Your User ID here
+KEY='your key here
+SECRET='your secret here' # Your Secret Here
+USER_ID='your id' # Your User ID here
 API_BASE_URL = "https://api.schoology.com/v1/users/"
